33b0fda112b0c84b7299530ebf20da4be5119dc5
efsa <113553752+efsacode@users.noreply.github.com>
2023-07-26 01:53:39 +0300

    last but not least, still on develop

--- myProject/movies/models.py
+++ myProject/movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+    anasayfa = models.BooleanField(default=False)
+    
--- myProject/movies/views.py
+++ myProject/movies/views.py
@@ -1,55 +1,56 @@
 from django.shortcuts import render
+from .models import Category, Movie
 
-categories = [
-    "Animation",
-    "Sci-Fi",
-    "Romantic"
-]
+# categories = [
+#     "Animation",
+#     "Sci-Fi",
+#     "Romantic"
+# ]
 
-filmler = [
-{   
-    "id":1,
-    "title": "movie_1",
-    "category": "Animation",
-    "image": "1.jpg",
-    "anasayfa": True,
-},
+# filmler = [
+# {   
+#     "id":1,
+#     "title": "movie_1",
+#     "category": "Animation",
+#     "image": "1.jpg",
+#     "anasayfa": True,
+# },
 
-{
-    "id":2, #id ekledik ve her bir film icin ayri ayri id tanimladik
-    "title": "movie_2",
-    "category": "Sci-Fi",
-    "image": "2.jpg",
-    "anasayfa": True,
-},
+# {
+#     "id":2, #id ekledik ve her bir film icin ayri ayri id tanimladik
+#     "title": "movie_2",
+#     "category": "Sci-Fi",
+#     "image": "2.jpg",
+#     "anasayfa": True,
+# },
 
-{
-    "id":3,
-    "title": "movie_3",
-    "category": "Romantic",
-    "image": "3.jpg",
-    "anasayfa": False,
-},
+# {
+#     "id":3,
+#     "title": "movie_3",
+#     "category": "Romantic",
+#     "image": "3.jpg",
+#     "anasayfa": False,
+# },
 
-]
+# ] 
 
 def home(request):
     data = {
-        "category": categories, #categories listesini data sozlugune ekledik ve key tanimladik
-        "movie": filmler
+        "category": Category.objects.all(), #categories listesini data sozlugune ekledik ve key tanimladik
+        "movie": Movie.objects.filter(anasayfa=True)
     }
     return render(request, "index.html", data)
 
 def movies(request):
     data = {
-        "category": categories, #categories listesini data sozlugune ekledik ve key tanimladik
-        "movie": filmler
+        "category": Category.objects.all(), #categories listesini data sozlugune ekledik ve key tanimladik
+        "movie": Movie.objects.all()
     }
     return render(request, "movies.html",data)
 
 def movie_details(request, id):
     data = {
-        "id": id,
+        "movie": Movie.objects.get(id=id),
     }
     return render(request, "details.html",data)
    
